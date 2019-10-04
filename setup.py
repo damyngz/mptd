1995c25b03f9c0a8e3eb839c1722f085312b6c9c
Lim Ming Wei <edward.mingwei@gmail.com>
2019-10-04 16:18:48 +0800

    added setup script, removed pyc files

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='mptd',
+    version='0.1.3',
+    packages=find_packages(),
+    package_dir={'': 'src'},
+    python_requires='>=3.6',
+    url='https://github.com/damyngz/mptd.git'
+)
